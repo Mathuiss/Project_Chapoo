97850a52ebffdebac87c14d45b8ba1cd413f2e6d
Unknown <mathijsgeerlings@gmail.com>
2018-05-17 12:32:28 +0200

    Create script + ERD v1

---
+++ docs/create.sql
@@ -0,0 +1,51 @@
+create database Chapoo
+go
+
+create table tafel
+(
+
+	id int not null,
+	aantalPersonen int not null,
+	isBezet boolean,
+
+	constraint Tafel_PK primary key (id)
+)
+
+create table gerecht
+(
+	id int not null primary key,
+	naam string not null,
+	prijs double not null,
+	categorie int,
+
+	constraint Gerecht_PK primary key (id)
+)
+
+create table gebruiler
+(
+	id int not null primary key,
+	gebruikersnaam string not null,
+	wachtwoord not null,
+	functie int,
+
+	constraint Gebruiker_PK primary key (id)
+)
+
+create table bestelling
+(
+	id int not null primary key,
+	gebruiker int foreign key references gebruiker (id),
+	tafel int foreign key references tafel (id),
+	status int not null,
+
+	constraint Bestelling_PK primary key (id)
+)
+
+create table inhoudBestelling
+(
+	idBestelling int foreign key references bestelling (id),
+	idGerecht int foreign key references gerecht (id),
+	
+	constraint BestellingId_PK primary key (idBestelling),
+	constraint GerechtId_PK primary key (idGerecht)
+)
