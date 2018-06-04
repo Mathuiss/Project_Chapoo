206aefd502fd9a4fe65c9ddd025de1deaf07d4a2
Unknown <mathijsgeerlings@gmail.com>
2018-06-04 10:46:47 +0200

    UI updates

---
+++ Chapoo/Model/Rekening.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapoo.Model
+{
+    public struct Rekening
+    {
+        public int RekeningNummer { get; set; }
+        public int Tafel { get; set; }
+        public List<Gerecht> Bestelling { get; set; }
+        public float TotaalPrijs { get; set; }
+
+        public Rekening(int _rekeningNummer, int _tafel, List<Gerecht> _bestelling, float _totaalPrijs)
+        {
+            RekeningNummer = _rekeningNummer;
+            Tafel = _tafel;
+            Bestelling = _bestelling;
+            TotaalPrijs = _totaalPrijs;
+        }
+    }
+}
